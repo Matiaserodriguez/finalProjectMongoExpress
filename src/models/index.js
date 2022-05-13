4e18882d9c0111ebbd6f80579389072c8e127b8e
Matias <maturod96@gmail.com>
2022-05-13 18:04:49 -0300

    Adds connection to the database and creates project structures

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const dbConfig = require('../db/db.config.js');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+// db.users = require('./users.js')(mongoose);
+// db.movies = require('./movies.js')(mongoose);
+// db.books = require('./books.js')(mongoose);
+// db.favorites = require('./favorites.js')(mongoose);
+
+module.exports = db;
