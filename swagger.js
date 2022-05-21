23e50b9053940d395da2764995018bc8dfa7a0d1
dalofulgence <dal19006@byui.edu>
2022-05-21 16:27:45 -0600

    swagger update

--- swagger.js
+++ swagger.js
@@ -5,12 +5,12 @@ const doc = {
     title: 'Favorite Medias',
     description: 'This is an API for the user to be able to store favorite bookas and movies',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'favorite-medias.herokuapp.com',
+  schemes: ['https'],
   securityDefinitions: {
     "oauth": {
       "type": "oauth2",
-      "authorizationUrl": "http://localhost:3000/login/oauth/authorize",
+      "authorizationUrl": "https://favorite-medias.herokuapp.com/login/oauth/authorize",
       "description":  "INSTRUCTIONS: 1) Click Authorize. 2) Authenticate with your account. 3) Get your access_token and copy it. 4) Paste it into any request you make.",
       "flow": "Follow instructions"
     }
