03010b42590449759fb3f39305070f00754c4ff7
WesGatner <wesgtr@gmail.com>
2022-05-26 19:27:41 -0300

    get by year and movieName

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,7 +15,7 @@ routes.use('/users', authorize.isAuthorized, user);
 routes.use('/login/oauth/authorize', oauth);
 routes.use('/api/session', accessToken.getAccessToken, token);
 routes.use('/books', authorize.isAuthorized, books);
-routes.use('/movies', movies);
+routes.use('/movies', authorize.isAuthorized, movies);
 
 // Swagger routes
 routes.use('/api-docs', swaggerUi.serve);
